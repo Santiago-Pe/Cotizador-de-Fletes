004bf60cf019249a9c2392200a4b02196edf904d
Santiago Peña <santiago.lae97@gmail.com>
2021-08-09 18:37:08 -0300

    Objetos-Nuevos

--- main.js
+++ main.js
@@ -1,47 +1,7 @@
-//Ejercictacion de Objetos.
+//OBJETO DE PROYECTO
 /*
-Cargas de personas para vacunar contra covid.
-
-1) Cargar las personas
-2) Definir por algun criterio, prioridad en la fila.
-3) Mostrar la lista prioritaria a vacunar.
-4) Mostrar porcentaje de personas a vacunar con prioridad.
+1- EL newslleter
+2- El form de contactp
+3- El form de compras
+4- el carrito de compras
 */
-
-class Persona{
-    constructor(pDNI,pNombre,pEdad,pEsencial){
-        this.dni = pDNI;
-        this.nombre = pNombre;
-        this.edad = pEdad;
-        this.esencial = pEsencial;
-
-        this.prioritario = pEdad >= 65 && pEsencial;
-    }
-}
-
-var acumuladorDeAtendidos = 0;
-var acumuladorPrioritarios = 0;
-
-while(true){
-    let pDNI = Number.parseInt(prompt("Ingresa tu DNI"));
-
-    if (isNaN(pDNI)) break; //Detenemos el bucle // Que es isNaN
-    let pNombre = Number.parseInt(prompt("Ingresa tu Nombre"));
-    let pEdad = Number.parseInt(prompt("Ingresa tu Edad"));
-    let pEsencial = Number.parseInt(prompt("Ingresa (s/n) si sos esencial") === "si");
-
-    var personalActual = new Persona(pDNI, pNombre, pEdad, pEsencial);
-
-    if(personalActual.prioritario){
-        console.log("La persona " + personalActual.nombre + "va a ser atendida en este momento");
-        acumuladorPrioritarios++;
-    }
-    acumuladorDeAtendidos++;
-
-}
-
-console.log (
-    "El total de personas atendidas es :" +
-    acumuladorDeAtendidos + "y el % de prioritarios es: " +
-    (acumuladorPrioritarios * 100) / acumuladorDeAtendidos
-);
