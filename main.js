7bbe35be7f53bd491a233e2db892a48df1a320d9
Santiago Peña <santiago.lae97@gmail.com>
2021-09-02 16:46:06 -0300

    cotizacion1

--- main.js
+++ main.js
@@ -47,15 +47,81 @@ let cliente = [] //Con el class Remitente.
 function calcularFlete(e)
 {
     e.preventDefault();
-    var
+    var km = 0;
+    var precioXKm = 140;
+    if (document.getElementsByClassName('.bsAs'))
+    {
+        km = 400;
+    }
+    if (document.getElementsByClassName('.capi'))
+    {
+        km = 300;
+    }
+    if (document.getElementsByClassName('.cata'))
+    {
+        km = 830;
+    }
+    if (document.getElementsByClassName('.chaco'))
+    {
+        km = 720;
+    }
+    if (document.getElementsByClassName('.chub'))
+    {
+        km = 1460;
+    }
+    if (document.getElementsByClassName('.cordo'))
+    {
+        km = 400;
+    }
+    if (document.getElementsByClassName('.corri'))
+    {
+        km = 740;
+    }
+    if (document.getElementsByClassName('.entreR'))
+    {
+        km = 200;
+    }
+    if (document.getElementsByClassName('.formo'))
+    {
+        km = 890;
+    }
+    //ampliar si funciona
+    costo = precioXKm * km;
+    return costo;
 }
+//revisar
 function validarForm ()
 {
+    // Remitente
+if ( nombreCliente == '' || apellidoCliente == '' ||telCliente == '')
+{
+    alert = ('Por favor complete bien los datos del Remitente')
+    datosOk = false;
+}
+// Receptor
+if ( nombreCliente2 == '' || apellidoCliente2 == '' ||telCliente2 == '')
+{
+    alert = ('Por favor complete bien los datos del Destinatario')
+    datosOk = false;
+}
+// Flete
+if ( producto == '' || cantidad == '' || valor == '' || peso == '')
+{
+    alert = ('Por favor complete bien los datos del Envio')
+    datosOk = false;
+}
+//Condciones de compra
+if ( producto == '' || cantidad == '' || valor == '' || peso == '')
+{
+  //Se tiene que seleccionar algo si o si.
+}
 
 }
 function agregarCliente ()
 {
-
+    //Si confimra presupuesto enviar un email y sumar a mi array. Crear una nueva funcion para el push de fletes, y ver de que manera se puede hacer que aumente mi ID por cada vez que un client me confirme una cotizacion
+    fletes.push('Rosario', destino, precio);
+    cliente.push('1', nombreCliente, apellidoCliente, telCliente)
 }
 function mostrarResuemn ()
 {
@@ -63,9 +129,35 @@ function mostrarResuemn ()
 }
 function agregarDom()
 {
+    let cotizacion = document.getElementsByClassName('cotizacion');
+    cotizacion.innerHTML =  `<h2> Su presupuesto</h2>
+    <h3> Datos del Remitente </h3>
+    <p><strong> Nombre : </strong> ${nombreCliente}</p>
+    <p><strong> Apellido : </strong> ${apellidoCliente}</p>
+    <p><strong> Tel : </strong> ${apellidoCliente}</p>
 
+    <h3> Datos del Destinatario </h3>
+    <p><strong> Nombre : </strong> ${nombreCliente2}</p>
+    <p><strong> Apellido : </strong> ${apellidoCliente2}</p>
+    <p><strong> Tel : </strong> ${apellidoCliente2}</p>
+
+    <h3> Datos del Envio </h3>
+    <p><strong> Producto : </strong> ${producto}</p>
+    <p><strong> Cantidad (en und de pallet): </strong> ${cantidad}</p>
+    <p><strong> Valor (en pesos) : </strong> ${valor}</p>
+    <p><strong> Peso en Kg : </strong> ${peso}</p>
+
+    <h3> Costo de envio </h3>
+    <p><strong> Costo de envio : </strong> ${costo2}</p>
+    <p><strong> + IVA : </strong> ${precio}</p> 
+    <button class = "btnAceptar"> Aceptar </buttoon>` //falta algo. Revisar;
 }
-
+function sumarIva()
+{
+    const iva = 1.21
+    let costoIva = costo2 * iva;
+    return costoIva;
+}
 //Constantes 
 //(Provincias con distancia a rosario.)
 // const buenosAires = 400;
@@ -96,9 +188,14 @@ function agregarDom()
 //Otras
 const iva = 1.21;
 const precioXkm = 140;
+
+//falta ver este tema, para la condicion del flete
 const camionCompleto = 1;
 const medioCamion = 0.8;
 const cuartoCamion = 0.6;
+let datosOk = true;
+costo2 = calcularFlete();
+let precio = sumarIva();
 //Aca en la cantidad, puedo determinarla yo con un maximo de pallet y hacer una formula para determianr 
 //que cantidad de pallet representa el espacio a ocupar. VEEER
 
@@ -127,7 +224,7 @@ let cantidad = miFormProd.children[3];
 let valor = miFormProd.children[5];
 let peso = miFormProd.children[7];
 
-//Formulario de condiciones de compra
+//Formulario de Envio
 const destinos = querySelector('.destino');
 destinos.addEventListenner('change') 
 
